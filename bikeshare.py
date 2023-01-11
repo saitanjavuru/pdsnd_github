e4157f8e148ffe3397f19028d2cff284966ce9f2
Sai Abhiroop Tanjavuru <saiabhiroop@yahoo.com>
2023-01-11 10:33:47 -0600

    Added descriptive comments for the station_stats function

--- bikeshare.py
+++ bikeshare.py
@@ -131,7 +131,14 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """
+    
+    Displays statistics on the most popular stations and trip.
+
+    Args:
+        df - Pandas DataFrame containing city data filtered by month and day
+    
+    """
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
